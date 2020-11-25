2ebe7aa611c0b921da33551b2604d2d0254a24f4
Nguyen Trong Tin <anglekiller9x@gmail.com>
2020-11-25 16:02:51 +0700

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+using System.Net.Http;
+using System.Net;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HttpRequest http = new HttpRequest();
+            string html = http.Get("https://google.vn/").ToString();
+            var request = (HttpWebRequest)WebRequest.Create("http://www.example.com/recepticle.aspx");
+            MessageBox.Show(html);
+        }
+    }
+}
